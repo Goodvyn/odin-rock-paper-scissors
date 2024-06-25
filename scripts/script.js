e6270a4e8e0b0c45d48a2cd19686522ccd8e4ba9
Nikita Kartavyi <28952277+Goodvyn@users.noreply.github.com>
2024-06-25 19:40:56 -0400

    update: add computer and human choice generation
    This adds random choice of rock,paper,scissors
    for the computer opponent and a manual input
    for the human player. The input is validated:
    empty and undefined entries, and everything that
    is not rock, paper or scissors, will prompt for
    the input again until its correct. The general
    message that lets the player know that something
    is wrong with the input is displayed as a prompt
    text.
    
    There are also variables to store computer and
    human scores, for now they are just defaulted to
    0.

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,64 @@
-console.log("Hello World");
+function getComputerChoice() {
+
+    const randomNum = generateRandNumber();
+    switch (randomNum) {
+        case 1:
+            return "Rock";
+        case 2:
+            return "Paper";
+        case 3:
+            return "Scissors";
+    }
+}
+
+function generateRandNumber() {
+    return Math.floor(Math.random() * 3) + 1;
+}
+
+function promptHumanInput(message) {
+
+    let humanInput = prompt(message);
+    return humanInput;
+}
+
+function validateHumanInput(humanInput) {
+    let isHumanInputValid = false;
+
+    if (humanInput == "" || humanInput === undefined) {
+        return isHumanInputValid;
+    }
+
+    switch (humanInput.toLowerCase()) {
+        case "rock":
+        case "paper":
+        case "scissors":
+            isHumanInputValid = true;
+            break;
+    }
+
+    return isHumanInputValid;
+}
+
+
+
+function getHumanChoice() {
+
+    let message = "Rock, paper or scissors? Enter your choice:";
+    let humanInput = promptHumanInput(message);
+
+    while (!validateHumanInput(humanInput)) {
+        message = "Oops. Make sure that your input isn't empty and is either rock, paper or scissors:";
+        humanInput = promptHumanInput(message);
+    }
+
+    return humanInput.toLowerCase();
+
+}
+let computerScore = 0;
+let humanScore = 0;
+
+const computerChoice = getComputerChoice();
+const humanChoice = getHumanChoice();
+
+console.log(computerChoice);
+console.log(humanChoice);
