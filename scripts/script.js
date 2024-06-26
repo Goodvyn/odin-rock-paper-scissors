efebd8dc8a2c7859e6588b019678a650dafa4dec
Nikita Kartavyi <28952277+Goodvyn@users.noreply.github.com>
2024-06-26 17:10:33 -0400

    update & fix: add ability to play one round
    Added playRound(), which checks the inputs
    and prints out the result (win, lose or draw) with
    the new printRoundResult() function.
    
    Made the error message in getHumanChoice() into a
    separate variable, rather than overwriting the
    "message" variable.
    
    Added formatHumanChoice() function to convert
    humanInput to lowercase and then to capitalize the
    first letter it for easier printing and
    comparisons.
    
    Fixed validateHumanInput() when checking if the
    input is null, previously didn't check for null
    at all.

--- scripts/script.js
+++ scripts/script.js
@@ -24,7 +24,7 @@ function promptHumanInput(message) {
 function validateHumanInput(humanInput) {
     let isHumanInputValid = false;
 
-    if (humanInput == "" || humanInput === undefined) {
+    if (humanInput === "" || humanInput === undefined || humanInput === null) {
         return isHumanInputValid;
     }
 
@@ -39,26 +39,105 @@ function validateHumanInput(humanInput) {
     return isHumanInputValid;
 }
 
+function formatHumanChoice(humanInput) {
 
+    humanInput = (humanInput.toLowerCase()).split("");
+    humanInput[0] = humanInput[0].toUpperCase();
+    return humanInput.join("");
+
+}
 
 function getHumanChoice() {
 
     let message = "Rock, paper or scissors? Enter your choice:";
+    let errorMessage = "Oops. Make sure that your input isn't empty and is either rock, paper or scissors:";
     let humanInput = promptHumanInput(message);
 
     while (!validateHumanInput(humanInput)) {
-        message = "Oops. Make sure that your input isn't empty and is either rock, paper or scissors:";
-        humanInput = promptHumanInput(message);
+        humanInput = promptHumanInput(errorMessage);
     }
 
-    return humanInput.toLowerCase();
+    return formatHumanChoice(humanInput);
 
 }
+
+function printRoundResult(computerChoice, humanChoice, isWin, isDraw) {
+    if (isDraw) {
+        console.log(`Its a draw! Both selected ${computerChoice}`);
+        return;
+    }
+
+    if (isWin) {
+        console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+        return;
+    } else {
+        console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+        return;
+    }
+}
+
+function playRound(computerChoice, humanChoice) {
+    if (computerChoice === humanChoice) {
+        printRoundResult(computerChoice, humanChoice, false, true);
+        return;
+    }
+
+    if (computerChoice === "Rock") {
+
+        if (humanChoice === "Paper") {
+            printRoundResult(computerChoice, humanChoice, true, false);
+        } else {
+            printRoundResult(computerChoice, humanChoice, false, false);
+        }
+    } else if (computerChoice === "Paper") {
+
+        if (humanChoice === "Scissors") {
+            printRoundResult(computerChoice, humanChoice, true, false);
+        } else {
+            printRoundResult(computerChoice, humanChoice, false, false);
+        }
+    } else if (computerChoice === "Scissors") {
+
+        if (humanChoice === "Rock") {
+            printRoundResult(computerChoice, humanChoice, true, false);
+        } else {
+            printRoundResult(computerChoice, humanChoice, false, false);
+        }
+    }
+
+}
+
+
+
 let computerScore = 0;
 let humanScore = 0;
+
+// const computerChoice = "Scissors";
 
 const computerChoice = getComputerChoice();
 const humanChoice = getHumanChoice();
 
-console.log(computerChoice);
-console.log(humanChoice);
+playRound(computerChoice, humanChoice);
+
+console.log(`Computer chose: ${computerChoice}`);
+console.log(`Human chose: ${humanChoice}`);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
